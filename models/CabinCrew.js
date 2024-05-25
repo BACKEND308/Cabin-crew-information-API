5dc2bacd70f830dc01bbccb6dd62d6c41be95ee6
Your Name <150568526+Shahd628@users.noreply.github.com>
2024-05-25 17:42:55 +0300

    Updated scripts

--- models/CabinCrew.js
+++ models/CabinCrew.js
@@ -1,16 +1,18 @@
+
+
 const mongoose = require('mongoose');
 
 const CabinCrewSchema = new mongoose.Schema({
-    CrewID: {type: Number, required: true, unique: true, default:'-'},
-    Role: {type: String, required: true, enum:['chief', 'regular', 'chef', '-'] , default:'-'},
-    MemberName: {type: String, required: true, default:'-'},
-    Age: {type: Number, required: true, default:0},
-    "Aircraft Restrictions": {type: Array, required: true, default:[]},
-    Assigned_Seat: {type: String, required: true, default:'-'},
-    Gender: {type:String, required: true, enum:['female', 'male', '-'], default:'-'},
-    "Known Languages": {type: Array, required: true, default:[]},
-    Nationality: {typr:String, required: true, default:'-'}
+    CrewID: {type: Number, required: true, unique: true, default: '-'},
+    Role: {type: String, required: true, enum: ['chief', 'regular', 'chef', '-'], default: '-'},
+    MemberName: {type: String, required: true, default: '-'},
+    AssignedSeat: {type: String, required: true, default: '-'},
+    Age: {type: Number, required: true, default: 0},
+    AircraftRestrictions: {type: Array, required: true, default: []},
+    Gender: {type: String, required: true, enum: ['female', 'male', '-'], default: '-'},
+    Known_Languages: {type: Array, required: true, default: []},
+    Nationality: {type: String, required: true, default: '-'}
 });
 
-const CabinCrew=mongoose.model('CabinCrew', CabinCrewSchema);
-module.exports=CabinCrew;
+const CabinCrew = mongoose.model('CabinCrew', CabinCrewSchema);
+module.exports = CabinCrew;
